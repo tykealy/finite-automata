a18748237b21a02d79170ec73a0f2c8de7f5485f
tk <tykeaboyloy@gmail.com>
2023-07-01 22:08:35 +0700

    added create and read method of firebase

--- src/app/new/page.jsx
+++ src/app/new/page.jsx
@@ -3,42 +3,62 @@ import "./../../../firebaseConfig";
 import Link from "next/link";
 import React from "react";
 import { ChevronLeftIcon } from "@heroicons/react/20/solid";
-import { getFirestore, updateDoc, doc } from "firebase/firestore";
+import { getFirestore, collection, addDoc } from "firebase/firestore";
 
-export async function update() {
+export async function createFA(name) {
   const firestore = getFirestore();
-  const testing = doc(firestore, "FA/1");
+  const c = collection(firestore, "automata");
   const doc1 = {
-    title: "NFA",
+    name: name,
   };
-  updateDoc(testing, doc1);
-  alert("Change saved to firebase");
+  try {
+    await addDoc(c, doc1);
+    alert(`New FA is created`);
+  } catch (error) {
+    console.log(error);
+  }
 }
+
 const Page = () => {
+  const [faName, setFaName] = React.useState("");
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    createFA(faName);
+    setFaName("");
+  };
+
   return (
-    <div>
+    <form onSubmit={handleFormSubmit}>
       <div className="max-w-6xl mx-auto my-7 px-4">
         <div className="flex items-center">
           <Link
             className="inline-flex items-center border rounded-lg bg-[#182c4c] mr-3 hover:bg-[#435f8c]"
             href="/"
+            as="/"
           >
             <ChevronLeftIcon className="h-8 w-8 text-white" />
             <span className="mr-2 text-white">Back</span>
           </Link>
-          <span className="flex-1 text-xl sm:text-2xl">
-            Design a Finite Automata
-          </span>
-          <span
-            onClick={update}
+          <input
+            name="fa-name"
+            type="text"
+            className="flex-1 text-xl sm:text-2xl border border-transparent focus:outline-none focus:ring-0"
+            placeholder="Input your FA Name"
+            onChange={(event) => {
+              setFaName(event.target.value);
+            }}
+          ></input>
+          <button
+            type="submit"
             className="text-white bg-[#182c4c] px-3 py-1 border rounded-lg hover:bg-[#435f8c]"
           >
             SAVE
-          </span>
+          </button>
         </div>
         <div className="border border-gray-300 my-3" />
       </div>
-    </div>
+    </form>
   );
 };
 
--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -5,7 +5,7 @@ const Topbar = () => {
   return (
     <div className=" flex overflow-hidden bg-gray-100 px-6 py-2.5 sm:px-3.5 md:px-7 ">
       <div className="w-64 sm:w-96">
-        <Image src={cadt} alt="cadt-logo"></Image>
+        <Image src={cadt} priority alt="cadt-logo"></Image>
       </div>
       <div className="flex flex-1 justify-end">
         <button
